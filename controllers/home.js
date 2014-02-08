188a5f7804143b10969d402d615da2f226f8cdcc
Brandon Wittwer <brandon@brandonwittwer.com>
2014-02-08 13:20:39 -0500

    homepage lists projects

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,17 @@
+var Project = require('../models/Project');
+
 /**
  * GET /
  * Home page.
  */
 
-exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+exports.index = function (req, res) {
+    Project.find().exec(function (err, projects) {
+        if (err) return next(err);
+        res.render('home', {
+            title: 'SocialGood I Owe',
+            projects: projects
+        });
+    });
+
 };
