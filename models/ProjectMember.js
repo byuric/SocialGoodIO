bca7a2f2c755424335a8601fd78c54d186140747
Brandon Wittwer <brandon@brandonwittwer.com>
2014-02-09 15:32:51 -0500

    removed description from project list

--- models/ProjectMember.js
+++ models/ProjectMember.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'), ObjectId = mongoose.Schema.Types.ObjectId;
 
 var projectMemberSchema = new mongoose.Schema({
-  user: { type: ObjectId, ref: 'Project' },
+  user: { type: ObjectId, ref: 'User' },
   role: { type: String, required: true }  //refactor to have a dictionary of availabe role types? (advocate, donator, volunteer)
 });
 
