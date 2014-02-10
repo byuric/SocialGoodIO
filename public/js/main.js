3859d0f7f9ae0e4838123108d108d8a180d6736d
Brandon Wittwer <brandon@brandonwittwer.com>
2014-02-09 21:49:49 -0500

    First pass at donations.  Not tracking the callback event yet, so nothing gets added to the db yet.  using test data you can get the experience.

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,33 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
     $('.js-projectMember-avatar, .js-projectMember').tooltip();
+  
+    document.helpers = function ($, document, StripeCheckout) {
+
+    var handler = StripeCheckout.configure({
+      key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+      image: '/img/logo.png',
+      token: function (token, args) {
+        console.log(token);
+        // Use the token to create the charge with a server-side script.
+      }
+    });
+    var checkout = function (name, desc, amt, emailAddress) {
+
+      // Open Checkout with further options
+      handler.open({
+        name: name,
+        description: desc,
+        amount: amt * 100.00,
+        email: emailAddress
+      });
+
+    }
+
+    return { checkout: checkout };
+
+
+  }($, document, StripeCheckout);
+  
 
 });
